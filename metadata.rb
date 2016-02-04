02013b0b8b90e55ec172f5870ffebfc0f69729a0
ocowchun <ocowchun@gmail.com>
2016-02-04 16:48:54 +0800

    adjust metadata
    add description

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,11 @@ name "sudo_elastic"
 version "0.1.3"
 maintainer "ocowchun"
 maintainer_email "ben.yeh@sudo.com.tw"
+license  "MIT"
 supports "ubuntu"
+description "install elastic and [elasticsearch-analysis-ik]"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 
-depends 'java'
-depends 'apt', '~> 2.9.2'
-depends 'elasticsearch', '~> 2.1.0'
+depends "java"
+depends "apt", "~> 2.9.2"
+depends "elasticsearch", "~> 2.1.0"
