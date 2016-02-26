0065330e6290a034c7cd2ae7a691ee3a1db163ee
ocowchun <ocowchun@gmail.com>
2016-02-26 16:44:20 +0800

    upgrade ik version to 1.8.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['java']['jdk_version']='7'
 default['sudo_elastic']['home_path']='/home/vagrant'
 default['sudo_elastic']['elastic_search']['network_host']='0.0.0.0'
-default['sudo_elastic']['ik']['version']='1.7.0'
+default['sudo_elastic']['ik']['version']='1.8.0'
 default['sudo_elastic']['elastic_search']['cluster']['name']='my_escluster'
 default['sudo_elastic']['elastic_search']['node']['name']='node1'
